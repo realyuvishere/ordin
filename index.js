c22ed3764f3a83d5b4c4c7dbd823971f5e861530
Yuv <billiondollarone07@gmail.com>
2019-05-16 00:24:24 +0530

    Aa

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
         scrollOverflow: true,
         menu: '#navb',
         recordHistory: false,
-        sectionsColor: ['#111', '#222', '#111', '#222', '#333'],
+        sectionsColor: ['#111', '#222', '#222', '#333'],
         afterRender: function(){
             $(".lds-css").fadeOut("slow");
             $('.intro').delay(1000).fadeOut("slow");
@@ -13,7 +13,7 @@ $(document).ready(function() {
             }, 2000);
             $(document).ready(function(){
                 $(".thisguy").click(function(){
-                    $("table").slideToggle();
+                    $(".eventhead").slideToggle();
                 });
             });
         },
@@ -32,11 +32,12 @@ var countDownDate = new Date("Aug 6, 2019 00:00:00").getTime();
 var x = setInterval(function() {
     var now = new Date().getTime();
     var distance = countDownDate - now;
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var months = Math.floor(distance / (1000 * 60 * 60 * 24 * 31));
+    var days = Math.floor((distance % (1000 * 60 * 60 * 24 * 31)) / (1000 * 60 * 60 * 24));
     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    document.getElementById("demo").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s "; 
+    document.getElementById("demo").innerHTML = months + "M " + days + "D " + hours + "h " + minutes + "m " + seconds + "s "; 
     if (distance < 0) {
         clearInterval(x);
         document.getElementById("demo").innerHTML = "Let's Roll ;)";
