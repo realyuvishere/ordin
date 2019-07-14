96434e4a0b879b082b7e8bde0d6bee02ced60005
Yuv <billiondollarone07@gmail.com>
2019-07-14 15:33:48 +0530

    News

--- index.js
+++ index.js
@@ -27,12 +27,11 @@ var countDownDate = new Date("Aug 6, 2019 00:00:00").getTime();
 var x = setInterval(function() {
     var now = new Date().getTime();
     var distance = countDownDate - now;
-    var months = Math.floor(distance / (1000 * 60 * 60 * 24 * 31));
     var days = Math.floor((distance % (1000 * 60 * 60 * 24 * 31)) / (1000 * 60 * 60 * 24));
     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    document.getElementById("demo").innerHTML = months + "M " + days + "D " + hours + "h " + minutes + "m " + seconds + "s "; 
+    document.getElementById("demo").innerHTML =  days + "D " + hours + "h " + minutes + "m " + seconds + "s "; 
     if (distance < 0) {
         clearInterval(x);
         document.getElementById("demo").innerHTML = "Let's Roll ;)";
